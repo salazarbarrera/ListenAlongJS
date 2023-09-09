d46b15cc04a4732d5aad27b4e5ed753abceb895c
salazarbarrera <diego.salazar.b@sabatech.cl>
2023-09-09 02:26:06 -0300

    Version 1.1.0: new UI and features
    * Removed Alert
    * Divided the page into steps so it is compatible with more browsers
    * Added text inputs for Google Api Key and target user so it is easier to user in basic hostings like neocities.
    * Allowing autocomplete of fields using a fake form hack.
    * Link to git and tutorial

--- src/index.js
+++ src/index.js
@@ -21,17 +21,19 @@
 * ********************************************************************************* */
 
 
-//////////////////////////////////////
-var search = require('youtube-search');
-var LastFmNode = require('lastfm').LastFmNode;
+////////////////////_HARDCODED_VARIABLES////////////////////
+var GOOGLE_KEY = 'GOOGLE_API_KEY';
+var USER = 'TARGET_USER';
+////////////////////////////////////////////////////////////
+
+
+////////////////////_IMPORTS_&_CONSTANTS////////////////////
+const search = require('youtube-search');
+const LastFmNode = require('lastfm').LastFmNode;
 const MAX_RESULTS = 30;
-const USER = 'TARGET_USER';
-//////////////////////////////////////
-const GOOGLE_KEY = 'GOOGLE_API_KEY';
 const lastfmSecret = 'd93696111a9e0618b674a327f666be5e'
 const lastfmApi = 'c6428f3b317e769153c76688dee205ad'
-
-//////////////////////////////////////
+////////////////////////////////////////////////////////////
 
 
 var player;
@@ -40,6 +42,7 @@ var oldTrack;
 
 var videoResults;
 var videoIndex;
+var lastfm
 
 function resumeLoadVideo(){
   if (videoIndex < MAX_RESULTS)
@@ -94,85 +97,115 @@ let token_url = "http://ws.audioscrobbler.com/2.0/?method=auth.gettoken&api_key=
 var mytoken;
 var mySession;
 
-$.getJSON( token_url, function( data ) {
-  mytoken = data.token;
 
-  authUrl = "https://www.last.fm/api/auth?api_key=" + lastfmApi + "&token=" + mytoken;
-  var win = window.open(authUrl, '_blank');
+function auth(){
+    lastfm = new LastFmNode({
+      api_key: lastfmApi,
+      secret: lastfmSecret
+    });
 
-      //Browser has blocked it
-      alert('Please allow popups for this website and authorize it to scrobble in your name.');
-      mySession = lastfm.session({
-       token: mytoken,
-       handlers: {
-          success: function(session) {
-            console.log("Success!");
-          }
-       }
-      });
-});
+    //alert('Please allow popups for this website and authorize it to scrobble in your name.');
+    $.getJSON( token_url, function( data ) {
+    mytoken = data.token;
 
+    authUrl = "https://www.last.fm/api/auth?api_key=" + lastfmApi + "&token=" + mytoken;
+    var win = window.open(authUrl, '_blank', 'toolbar=yes,scrollbars=yes,width=500,height=400');
+    $("#lobby").hideV();
+    $("#launch").showV();
 
-var lastfm = new LastFmNode({
-  api_key: lastfmApi,
-  secret: lastfmSecret
-});
-
-
-
-var opts = {
-  maxResults: MAX_RESULTS,
-  key: GOOGLE_KEY
-};
-
-
-
-
-var trackStream = lastfm.stream(USER);
-
-var titulo = "";
-
-console.log(trackStream.name);
-
-trackStream.on('lastPlayed', function(track) {
-/*  console.log('Last played: ' + (track.artist)['#text'] + " - " + track.name);
-*/});
-
-trackStream.on('nowPlaying', function(track) {
-  console.log('Now playing: ' + (track.artist)['#text'] + " - " + track.name);
-
-  document.getElementById("song").innerHTML = 'Now playing: ' + (track.artist)['#text'] + " - " + track.name;
-
-  search((track.artist)['#text'] + " - " + track.name, opts , function (err, results) {
-  if(err) return console.log(err);
-
-  console.dir("ID: " + results[0].id);
-    loadVideo(results, 0);
-    if (oldDate != null){
-      lastfm.update('scrobble', mySession, { track: oldTrack, timestamp: oldDate });
-    }
-    oldDate = Math.floor(Date.now() / 1000);
-    oldTrack = track;
-    lastfm.update('nowplaying', mySession, { track: track } );
   });
+}
+
+function launch(){
+  opts = {
+    maxResults: MAX_RESULTS,
+    key: GOOGLE_KEY
+  };
+
+
+  mySession = lastfm.session({
+   token: mytoken,
+   handlers: {
+      success: function(session) {
+        console.log("Success!");
+      }
+   }
+  });
+
+  var trackStream = lastfm.stream(USER);
+
+  var titulo = "";
+
+  console.log(trackStream.name);
+
+  trackStream.on('lastPlayed', function(track) {
+  /*  console.log('Last played: ' + (track.artist)['#text'] + " - " + track.name);
+  */});
+
+  trackStream.on('nowPlaying', function(track) {
+    console.log('Now playing: ' + (track.artist)['#text'] + " - " + track.name);
+
+    document.getElementById("song").innerHTML = 'Now playing: ' + (track.artist)['#text'] + " - " + track.name;
+
+    search((track.artist)['#text'] + " - " + track.name, opts , function (err, results) {
+    if(err) return console.log(err);
+
+    console.dir("ID: " + results[0].id);
+      loadVideo(results, 0);
+      if (oldDate != null){
+        lastfm.update('scrobble', mySession, { track: oldTrack, timestamp: oldDate });
+      }
+      oldDate = Math.floor(Date.now() / 1000);
+      oldTrack = track;
+      lastfm.update('nowplaying', mySession, { track: track } );
+    });
+  });
+
+  trackStream.on('scrobbled', function(track) {
+  /*  console.log('Scrobbled: ' + track.name);
+  */});
+
+  trackStream.on('stoppedPlaying', function(track) {
+    console.log('Stopped playing: ' + track.name);
+    if (oldDate != null){
+        lastfm.update('scrobble', mySession, { track: oldTrack, timestamp: oldDate });
+    }
+  });
+
+  trackStream.on('error', function(error) {
+    console.log('Error: '  + error.message);
+  });
+
+  trackStream.start();
+
+}
+
+jQuery.fn.showV = function() {
+    this.css('display', 'block');
+}
+
+jQuery.fn.hideV = function() {
+    this.css('display', 'none');
+}
+
+
+$(document).ready(function() {
+    $("#auth").click(function() {
+        GOOGLE_KEY = document.getElementById("googleKeyUI").value == "" ? GOOGLE_KEY : document.getElementById("googleKeyUI").value ;
+        USER = document.getElementById("targertUserGUI").value == "" ? USER : document.getElementById("targertUserGUI").value;
+        setTargetUser();
+        document.querySelector('form button').click();
+        auth();
+    });
 });
 
-trackStream.on('scrobbled', function(track) {
-/*  console.log('Scrobbled: ' + track.name);
-*/});
-
-trackStream.on('stoppedPlaying', function(track) {
-  console.log('Stopped playing: ' + track.name);
-  if (oldDate != null){
-      lastfm.update('scrobble', mySession, { track: oldTrack, timestamp: oldDate });
-  }
+$(document).ready(function() {
+    $("#launch").click(function() {
+        launch();
+        $("#launch").hideV();
+        $("#player").showV();
+    });
 });
-
-trackStream.on('error', function(error) {
-  console.log('Error: '  + error.message);
-});
-
-trackStream.start();
 
 $(document).ready(function() {
     $("#skipper").click(function() {
@@ -180,10 +213,15 @@ $(document).ready(function() {
     });
 });
 
-targetUserUrl = "https://www.last.fm/user/" + USER;
-document.getElementById("targetuser").innerHTML = targetUserUrl;
-$(document).ready(function() {
-    $("#targetuser").click(function() {
-      window.open(targetUserUrl, '_blank');
-    });
-});
+
+function setTargetUser(){
+  targetUserUrl = "https://www.last.fm/user/" + USER;
+  document.getElementById("targetuser").innerHTML = targetUserUrl;
+  $(document).ready(function() {
+      $("#targetuser").click(function() {
+        window.open(targetUserUrl, '_blank');
+      });
+  });  
+}
+
+setTargetUser();
